de54bd2b37f99d6f662a4f758d7f1173e0c032a1
sinamong0620 <sinaskim60@gmail.com>
2023-08-06 23:16:57 +0900

    [P1N] 133499.js

---
+++ sina/230810/133499.js
@@ -0,0 +1,13 @@
+function solution(babbling) {
+  let answer = 0;
+
+  for (let word of babbling) {
+    if (/(aya|ye|woo|ma)\1+/g.test(word)) continue;
+
+    if (/^(aya|ye|woo|ma)+$/g.test(word)) {
+      answer++;
+    }
+  }
+
+  return answer;
+}
