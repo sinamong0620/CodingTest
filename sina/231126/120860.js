31bb9aedc4fd75593083b26ef42cc714b14c84a4
sinamong0620 <sinaskim60@gmail.com>
2023-11-26 21:06:32 +0900

    [P0C] 120860.js

---
+++ sina/231126/120860.js
@@ -0,0 +1,11 @@
+function solution(dots) {
+  const x = [];
+  const y = [];
+
+  for (let dot of dots) {
+    x.push(dot[0]);
+    y.push(dot[1]);
+  }
+
+  return (Math.max(...x) - Math.min(...x)) * (Math.max(...y) - Math.min(...y));
+}
